59c034833607dd740bd1f28930f1528659156e5e
Richard Tetteh <74477960+rich4000@users.noreply.github.com>
2026-03-26 05:37:42 +0000

    Create migration for job_skills table

---
+++ database/migrations/2026_03_26_053646_create_job_skills_table.php
@@ -0,0 +1,29 @@
+use Illuminate\Database\Migrations\Migration;
+
+class CreateJobSkillsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('job_skills', function (Blueprint $table) {
+            $table->id();
+            $table->string('skill_name');
+            $table->foreignId('job_id')->constrained()->onDelete('cascade');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('job_skills');
+    }
+}
